848d2d2c1cd6ebeb866b5ebc4f395c5b38c0880f
Hu Shuai <hus.fnst@cn.fujitsu.com>
2020-11-02 15:57:19 +0800

    test os10_command
    Signed-off-by: Hu Shuai <hus.fnst@cn.fujitsu.com>

--- tests/unit/plugins/modules/test_os10_command.py
+++ tests/unit/plugins/modules/test_os10_command.py
@@ -36,7 +36,7 @@ class TestDellos10CommandModule(TestDellos10Module):
     def setUp(self):
         super(TestDellos10CommandModule, self).setUp()
 
-        self.mock_run_commands = patch('ansible.modules.network.os10.os10_command.run_commands')
+        self.mock_run_commands = patch('ansible_collections.dellemc.os10.plugins.modules.os10_command.run_commands')
         self.run_commands = self.mock_run_commands.start()
 
     def tearDown(self):
@@ -64,6 +64,7 @@ class TestDellos10CommandModule(TestDellos10Module):
     def test_os10_command_simple(self):
         set_module_args(dict(commands=['show version']))
         result = self.execute_module()
+        print(result)
         self.assertEqual(len(result['stdout']), 1)
         self.assertTrue(result['stdout'][0].startswith('Dell EMC Networking'))
 
